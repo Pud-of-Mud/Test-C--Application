24248af7f3d933b7ce2fbfafafe32123fba8654c
Matt <109466200+Pud-of-Mud@users.noreply.github.com>
2023-01-30 15:03:46 +0000

    Trying to make the main window event handler

--- .ccls-cache/@home@runner@Test-C-application/main.cpp
+++ .ccls-cache/@home@runner@Test-C-application/main.cpp
@@ -1,6 +1,19 @@
-#include<isotream.h>
+#include <iostream>
 
 int main() {
-   
-   return 0;
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Works!");
+
+  while (window.isOpen()) {
+    sf:Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+
+      window.clear();
+      window.display();
+    }
+
+    return 0;
+  }
 }
